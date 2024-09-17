a33fe1ac3c92c71bcd64ffa8beb21231a94cbfb4
Yucheng-An <yuc.an2019@gmail.com>
2024-09-17 10:19:52 -0700

    Auto-commit JS on save

--- Temportary.js
+++ Temportary.js
@@ -16,7 +16,7 @@ req.onsuccess = function(event){
     let transactionBook = db.getObjectStore("book").transaction("book", "readwrite")
     let bookObjectStore = transcationBook.getObjectStore("book")
     let book = {
-        uuid: cropUUID()
+        uuid: cropto
         title: "Harry Potter",
     }
 
