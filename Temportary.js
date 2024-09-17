cd6609b81efdbf72fb81acdc14056385090c73ee
Yucheng-An <yuc.an2019@gmail.com>
2024-09-17 10:12:20 -0700

    Auto-commit JS on save

--- Temportary.js
+++ Temportary.js
@@ -1,9 +1,6 @@
 // create the indexedDB
 let req = indexedDB.open("BookDB",1)
-req.onsuccess = function(event){
-    let db = event.target.result
 
-}
 
 // trigger when creating database first time
 req.onupgradeneeded = function(event){
