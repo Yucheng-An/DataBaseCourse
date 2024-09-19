1d237bf511eae5116d300d8075a0b75b38448af8
Yucheng-An <yuc.an2019@gmail.com>
2024-09-19 10:51:00 -0700

    Auto-commit JS on save

---
+++ Lab02/index.js
@@ -0,0 +1,9 @@
+const vehicles = [ {"id": "V001", "make": "Toyota", "model": "Camry", "year": 2020, "mileage": 15000, "fuelType": "Gasoline", "color": "Blue", "owner": "John Doe", "lastServiceDate": "2024-01-15", "isElectric": false}, {"id": "V002", "make": "Honda", "model": "Civic", "year": 2019, "mileage": 25000, "fuelType": "Gasoline", "color": "Red", "owner": "Jane Smith", "lastServiceDate": "2024-03-10", "isElectric": false}, {"id": "V003", "make": "Tesla", "model": "Model 3", "year": 2022, "mileage": 5000, "fuelType": "Electric", "color": "White", "owner": "Emily Johnson", "lastServiceDate": "2024-06-22", "isElectric": true}, {"id": "V004", "make": "Ford", "model": "F-150", "year": 2018, "mileage": 60000, "fuelType": "Diesel", "color": "Black", "owner": "Michael Brown", "lastServiceDate": "2024-02-08", "isElectric": false}, {"id": "V005", "make": "Chevrolet", "model": "Bolt EV", "year": 2021, "mileage": 12000, "fuelType": "Electric", "color": "Green", "owner": "Sarah Smith", "lastServiceDate": "2024-07-30", "isElectric": true} ];
+function getVehicleSumMilage(vehicles) {
+    let sum = 0;
+    for (let i = 0; i < vehicles.length; i++) {
+        sum += vehicles[i].mileage;
+    }
+    return sum;
+}
+console.log(getVehicleSumMilage(vehicles));
