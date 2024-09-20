efcd2d0cca6e111d08d0439e5e34b6a5a0c78ed7
Yucheng-An <yuc.an2019@gmail.com>
2024-09-20 15:17:38 -0700

    Auto-commit JS on save

--- Lab02/index.js
+++ Lab02/index.js
@@ -45,8 +45,8 @@ function checkVilde(vehicles) {
 }
 
 function verifyYear(year) {
-    const currentYear = new Date().getFullYear(); // Get the current year
-    return typeof year === 'number' && year >= 1886 && year <= currentYear;
+    const currentYear = new Date().getFullYear()
+    return typeof year === 'number' && year >= 1886 && year <= currentYear
 }
 function verifyMake(make) {
     const allowedMakes = ["Toyota", "Honda", "Tesla", "Ford", "Chevrolet"]; // Define allowed makes
