37d81a230a7083cb87a82f3c99e63ac4a6c9e7b3
Yucheng-An <yuc.an2019@gmail.com>
2024-09-20 15:21:57 -0700

    Auto-commit JS on save

--- Lab02/index.js
+++ Lab02/index.js
@@ -59,5 +59,5 @@ function checkYear(info) {
 }
 function checkLSD(info) {
     const regex = /^\d{4}-\d{2}-\d{2}$/; // Regular expression for YYYY-MM-DD format
-    if (!regex.test(info)) return false; // Check format
+    return regex.test(info) && ; // Check format
 }
