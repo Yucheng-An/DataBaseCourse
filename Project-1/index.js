d15b02a5ddd477fd5b262710d0a3cea0c2f1c9a5
Yucheng-An <yuc.an2019@gmail.com>
2024-10-04 23:37:53 -0700

    Auto-commit JS on save

--- Project-1/index.js
+++ Project-1/index.js
@@ -46,22 +46,17 @@ const tasks = [
 
 function setupIndexedDB(dbName, storeName, callback) {
     let request = indexedDB.open(dbName, 1);
-
     request.onupgradeneeded = function (event) {
         let db = event.target.result;
-
-        // Create object store and index if they don't already exist
         if (!db.objectStoreNames.contains(storeName)) {
             let objectStore = db.createObjectStore(storeName, { keyPath: "id" });
             objectStore.createIndex("name", "name", { unique: false });
         }
     };
-
     request.onsuccess = function (event) {
         let db = event.target.result;
         callback(db);
     };
-
     request.onerror = function (event) {
         console.error("Error opening IndexedDB:", event);
     };
