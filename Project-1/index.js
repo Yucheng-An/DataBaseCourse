66a1c85df6a075beff00998df36218220f3983ac
Yucheng-An <yuc.an2019@gmail.com>
2024-10-04 23:05:28 -0700

    Auto-commit JS on save

--- Project-1/index.js
+++ Project-1/index.js
@@ -1,9 +1,7 @@
 function setupIndexedDB(dbName, storeName, callback) {
     let request = indexedDB.open(dbName, 1);
-
     request.onupgradeneeded = function (event) {
         let db = event.target.result;
-
         // Create object store and index if they don't already exist
         if (!db.objectStoreNames.contains(storeName)) {
             let objectStore = db.createObjectStore(storeName, { keyPath: "id" });
