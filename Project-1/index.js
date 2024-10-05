a395849c8602b276a7d501b716dafb72d830610e
Yucheng-An <yuc.an2019@gmail.com>
2024-10-04 23:08:45 -0700

    Auto-commit JS on save

--- Project-1/index.js
+++ Project-1/index.js
@@ -16,3 +16,38 @@ function setupIndexedDB(dbName, storeName, callback) {
     };
 }
 
+// Function to add 100k objects only if they don't already exist
+function add100kObjects(db, storeName, callback) {
+    let transaction = db.transaction(storeName, "readonly");
+    let objectStore = transaction.objectStore(storeName);
+    let countRequest = objectStore.count();
+
+    countRequest.onsuccess = function () {
+        // Only add objects if the store is empty
+        if (countRequest.result === 0) {
+            let writeTransaction = db.transaction(storeName, "readwrite");
+            let writeObjectStore = writeTransaction.objectStore(storeName);
+
+            for (let i = 0; i < 100000; i++) {
+                let object = { id: i, name: `Object_${i}`, value: `SomeValue_${i}` };
+                writeObjectStore.add(object);
+            }
+
+            writeTransaction.oncomplete = function () {
+                console.log("100k objects added.");
+                callback();
+            };
+
+            writeTransaction.onerror = function (event) {
+                console.error("Error adding objects:", event);
+            };
+        } else {
+            console.log("Objects already exist, skipping insertion.");
+            callback();
+        }
+    };
+
+    countRequest.onerror = function (event) {
+        console.error("Error counting objects:", event);
+    };
+}
