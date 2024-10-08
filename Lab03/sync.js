f36de4267e6c0f60a182bc03e324c5b1ff4cab84
Yucheng-An <yuc.an2019@gmail.com>
2024-10-07 22:06:36 -0700

    Auto-commit JS on save

--- Lab03/sync.js
+++ Lab03/sync.js
@@ -6,14 +6,14 @@ const uri = "mongodb+srv://i40:dbms2@cluster0.lixbqmp.mongodb.net/";
 const dbName = "lab3";
 const collectionName = "4449";
 
-// Function to fetch MongoDB data
-async function getMongoDBData(client) {
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
-    const mongoData = await collection.find({}); // Fetch all data from MongoDB
-    return mongoData;
-}
-console.log(getMongoDBData())
+// // Function to fetch MongoDB data
+// async function getMongoDBData(client) {
+//     const db = client.db(dbName);
+//     const collection = db.collection(collectionName);
+//     const mongoData = await collection.find({}); // Fetch all data from MongoDB
+//     return mongoData;
+// }
+// console.log(getMongoDBData())
 
 
 async function getIndexedDBData() {
@@ -25,4 +25,4 @@ async function getIndexedDBData() {
         return [];
     }
 }
-// console.log(getIndexedDBData())
+/console.log(getIndexedDBData())
