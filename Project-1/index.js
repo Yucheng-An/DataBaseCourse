2d74014be44eae4dcc27469a6caa02b1edb111b6
Yucheng-An <yuc.an2019@gmail.com>
2024-10-08 02:33:03 -0700

    Auto-commit JS on save

--- Project-1/index.js
+++ Project-1/index.js
@@ -149,7 +149,7 @@ function setAllStatus(db, storeName, status, callback) {
             cursor.update(updatedValue);
             cursor.continue();
         } else {
-            console.log("Set all Status update completed.");
+            console.log("All status update completed.");
             callback();
         }
     };
