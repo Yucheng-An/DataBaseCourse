31c3476513594f84b18a0015add4e2114b9d7111
Yucheng-An <yuc.an2019@gmail.com>
2024-10-07 23:25:34 -0700

    Auto-commit JS on save

--- Lab03/sync.js
+++ Lab03/sync.js
@@ -1,10 +1,17 @@
 const { MongoClient } = require('mongodb');
 const fs = require('fs');
 const uri = "mongodb+srv://i40:dbms2@cluster0.lixbqmp.mongodb.net/lab3";
+
 function readIndexDBJSONFile() {
-    const data = fs.readFileSync('browserIndexDBSensors', 'utf8');
-    return JSON.parse(data);
+    try {
+        const data = fs.readFileSync('browserIndexDBSensors', 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        console.error('Error reading the IndexedDB JSON file:', err);
+        return null;
+    }
 }
+
 async function syncDataToMongoDB() {
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     try {
@@ -12,18 +19,31 @@ async function syncDataToMongoDB() {
         const db = client.db('lab3');
         const collection = db.collection('4449');
         const indexDBData = readIndexDBJSONFile();
-        for (const obj of indexDBData) {
-            await collection.updateOne(
-                { uuid: obj.uuid },
-                { $set: obj },
-                { upsert: true }
-            );
+
+        if (!indexDBData) {
+            console.error('No data to sync.');
+            return;
         }
-        console.log('IndexedDB synced to MongoDB.');
+
+        const bulkOps = indexDBData.map(obj => ({
+            updateOne: {
+                filter: { uuid: obj.uuid },
+                update: { $set: obj },
+                upsert: true
+            }
+        }));
+
+        if (bulkOps.length > 0) {
+            await collection.bulkWrite(bulkOps);
+            console.log('IndexedDB synced to MongoDB.');
+        } else {
+            console.log('No documents to update or insert.');
+        }
     } catch (err) {
         console.error('Error syncing data to MongoDB:', err);
     } finally {
         await client.close();
     }
 }
+
 syncDataToMongoDB().then(r => console.log('Done'));
