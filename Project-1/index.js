188a4cb44ffb0b090c6125a87bfde1a0f7f16784
Yucheng-An <yuc.an2019@gmail.com>
2024-10-08 01:25:21 -0700

    Auto-commit JS on save

--- Project-1/index.js
+++ Project-1/index.js
@@ -96,6 +96,8 @@ function add100kObjects(db, storeName, callback) {
     };
 }
 
+
+
 // Function readingCompletedObject: Reading 1000 completed objects
 function readingCompletedObject(db, storeName, callback) {
     let transaction = db.transaction(storeName, "readwrite");
