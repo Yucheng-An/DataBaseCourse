13970f4259ce60eb364ecf336e0188273651383f
Yucheng-An <yuc.an2019@gmail.com>
2024-10-07 22:07:42 -0700

    Auto-commit JS on save

--- Lab03/sync.js
+++ Lab03/sync.js
@@ -18,7 +18,7 @@ const collectionName = "4449";
 
 async function getIndexedDBData() {
     try {
-        const response = await axios.get('https://se4cps.github.io/lab/dms2/#/250');
+        const response = await axios.get('http://localhost:63343/Database%20Managment%20Course/Lab03/app.html?_ijt=3evqdkoj2i9t4i8rbfna1igfdo&_ij_reload=RELOAD_ON_SAVE');
         return response.data; // Assuming the data is returned in the response body
     } catch (error) {
         console.error("Error fetching IndexedDB data: ", error);
