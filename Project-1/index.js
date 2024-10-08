a006bb5347c564fc67a8961b9e7a1cb7dc034544
Yucheng-An <yuc.an2019@gmail.com>
2024-10-08 02:15:25 -0700

    Auto-commit JS on save

--- Project-1/index.js
+++ Project-1/index.js
@@ -237,7 +237,7 @@ function measurePerformance() {
             console.log(`Time to read all objects with readonly transaction: ${endTimeRT - startTimeRT} ms`);
         });
 
-        Create an index on the `status` field, then measure and display the time to read all completed tasks on the console or the browser
+        //4. Create an index on the `status` field, then measure and display the time to read all completed tasks on the console or the browser
 
 
 
