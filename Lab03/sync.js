b4ec326895ef97d00735bbd5683e25052c2bf105
Yucheng-An <yuc.an2019@gmail.com>
2024-10-07 22:38:24 -0700

    Auto-commit JS on save

--- Lab03/sync.js
+++ Lab03/sync.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose').set('strictQuery', true)
 const url = 'mongodb+srv://i40:dbms2@cluster0.lixbqmp.mongodb.net/lab3?retryWrites=true&w=majority'
-const taskSchema = new mongoose.Schema({
+const sensorSchema = new mongoose.Schema({
     content: String,
     date: Date,
     important: Boolean,
