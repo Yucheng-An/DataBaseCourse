e3373b5c42d7a376c807088d5f0e665aa75a32d1
Yucheng-An <yuc.an2019@gmail.com>
2024-10-08 02:08:23 -0700

    Auto-commit JS on save

--- Project-1/index.js
+++ Project-1/index.js
@@ -180,9 +180,9 @@ function measurePerformance() {
     setupIndexedDB(dbName, storeName, function (db) {
         // add 100k objects
         add100kObjects(db, storeName, function () {});
-        // Reading 1000 completed objects
+        // 1. Reading 1000 completed objects
         setSomeStatus(db, storeName, 1000, 0, 100000-1000, function () {});
-        
+
 
     });
 }
