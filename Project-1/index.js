f73dd326127f2882fc580fd0024c4acf4a4df4f9
Yucheng-An <yuc.an2019@gmail.com>
2024-10-08 02:16:14 -0700

    Auto-commit JS on save

--- Project-1/index.js
+++ Project-1/index.js
@@ -240,7 +240,8 @@ function measurePerformance() {
         //4. Create an index on the `status` field, then measure and display the time to read all completed tasks on the console or the browser
 
 
-        // 5. Define a new object store called "TodoListCompleted", copy all completed tasks from "TodoList" to this new store, and measure and display the time required to read all completed tasks from "TodoListCompleted" on the console or the browser
+        // 5. Define a new object store called "TodoListCompleted", copy all completed tasks from "TodoList" to this new store,
+        // and measure and display the time required to read all completed tasks from "TodoListCompleted" on the console or the browser
 
 
 
