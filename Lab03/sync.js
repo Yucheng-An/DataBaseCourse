823f67189a998b5bfd6bb1d01b5ab7cba53dec46
Yucheng-An <yuc.an2019@gmail.com>
2024-10-07 22:04:28 -0700

    Auto-commit JS on save

--- Lab03/sync.js
+++ Lab03/sync.js
@@ -2,7 +2,7 @@ const { MongoClient } = require('mongodb');
 const axios = require('axios'); // To fetch IndexedDB data through an API
 
 // MongoDB connection details
-const uri = "mongodb+srv://i40:dbms2@cluster0.lixbqmp.mongodb.net/lab3";
+const uri = "mongodb+srv://i40:dbms2@cluster0.lixbqmp.mongodb.net/";
 const dbName = "lab3";
 const collectionName = "4449";
 
