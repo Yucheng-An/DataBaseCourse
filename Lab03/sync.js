f79cb746121e751a227e2245cf47cffea48641a0
Yucheng-An <yuc.an2019@gmail.com>
2024-10-07 23:38:23 -0700

    Auto-commit JS on save

--- Lab03/sync.js
+++ Lab03/sync.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 const { indexedDB, IDBKeyRange } = require('fake-indexeddb');
 
-const mongoUri = "mongodb+srv://i40:dbms2@cluster0.lixbqmp.mongodb.net/lab3";
+const mongoUri = "mongodb://localhost:27017//lab3";
 const mongoClient = new MongoClient(mongoUri);
 const mongoDbName = "lab3";
 const mongoCollectionName = "4449";
