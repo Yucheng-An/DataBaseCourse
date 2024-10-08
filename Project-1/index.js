accec75e161de4411bd869a4a233768f8a5cb812
Yucheng-An <yuc.an2019@gmail.com>
2024-10-08 01:34:18 -0700

    Auto-commit JS on save

--- Project-1/index.js
+++ Project-1/index.js
@@ -97,7 +97,7 @@ function add100kObjects(db, storeName, callback) {
 }
 
 // ----------------- 1.Set 1000 objects to status "completed" and the remaining ones to status "progress" -----------------
-function set1000CompletedRe(db, storeName, callback) {
+function set1000CompletedRemaningPro(db, storeName, callback) {
     let transaction = db.transaction(storeName, "readwrite");
     let objectStore = transaction.objectStore(storeName);
     let completedObjects = [];
