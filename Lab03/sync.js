8c7f7662a5ce6a7442551e1182be77cde0533691
Yucheng-An <yuc.an2019@gmail.com>
2024-10-07 23:14:27 -0700

    Auto-commit JS on save

--- Lab03/sync.js
+++ Lab03/sync.js
@@ -1,17 +1,29 @@
-const { MongoClient } = require('mongodb');
-const uri = 'mongodb+srv://i40:dbms2@cluster0.lixbqmp.mongodb.net/lab3';
-const client = new MongoClient(uri);
-async function readData() {
-    try {
-        await client.connect();
-        const database = client.db('lab3');
-        const collection = database.collection('4449');
-        const results = await collection.find({});
-        console.log('Documents:', results);
-    } catch (error) {
-        console.error('Error reading data:', error);
-    } finally {
-        await client.close();
-    }
+async function indexDBExport() {
+    return new Promise((resolve, reject) => {
+        const request = indexedDB.open('IndexDB', 1);
+        request.onsuccess = async (event) => {
+            const db = event.target.result;
+            const transaction = db.transaction('Sensor', 'readonly');
+            const objectStore = transaction.objectStore('Sensor');
+            const allIndexDBData = objectStore.getAll();
+            allIndexDBData.onsuccess = function () {
+                const data = allIndexDBData.result;
+                const jsonData = JSON.stringify(data, null, 2);
+                const blob = new Blob([jsonData], { type: "application/json" });
+                const temp1 = document.createElement("a");
+                temp1.href = URL.createObjectURL(blob);
+                temp1.download = "browserIndexDBSensors.json";
+                document.body.appendChild(temp1);
+                temp1.click();
+                document.body.removeChild(temp1);
+                resolve();
+            };
+            allIndexDBData.onerror = function () {
+                reject('Failed to retrieve data from IndexedDB.');
+            };
+        };
+        request.onerror = function (event) {
+            reject('Error opening IndexedDB: ' + event.target.errorCode);
+        };
+    });
 }
-readData();
