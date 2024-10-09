c915a4318880e06044b1cc2ad35ac5b4631893e0
Yucheng-An <yuc.an2019@gmail.com>
2024-10-08 20:29:40 -0700

    Auto-commit JS on save

--- Project-1/index.js
+++ Project-1/index.js
@@ -157,7 +157,7 @@ function readSomeStatusWithReadWrite(db, storeName, status, callback) {
     };
 }
 
-function readSomeStatusWithReadOnly(db, storeName, callback) {
+function readSomeStatusWithReadOnly(db, storeName, status ,callback) {
     let transaction = db.transaction(storeName, "readonly");
     let objectStore = transaction.objectStore(storeName);
     let count = 0;
