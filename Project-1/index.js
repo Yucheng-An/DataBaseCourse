40b2710f080cc3e23fa8d335d73c7574f524c848
Yucheng-An <yuc.an2019@gmail.com>
2024-10-10 03:42:02 -0700

    Auto-commit JS on save

--- Project-1/index.js
+++ Project-1/index.js
@@ -211,6 +211,7 @@ function main() {
                             console.log(`Time to read all objects with index: ${(endTime - startTime).toFixed(2)} ms`);
                             // 5. Define a new object store called "TodoListCompleted", copy all completed tasks from "TodoList" to this new store,
                             // and measure and display the time required to read all completed tasks from "TodoListCompleted" on the console or the browser
+                            
                         });
                     });
                 });
