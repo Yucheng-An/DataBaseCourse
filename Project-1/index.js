725c107d748b29cb286727fda63e2468f81b5fe6
Yucheng-An <yuc.an2019@gmail.com>
2024-10-10 03:40:36 -0700

    Auto-commit JS on save

--- Project-1/index.js
+++ Project-1/index.js
@@ -204,7 +204,12 @@ function main() {
                     readSomeStatusWithSomeMethod(db, storeName, "completed", "readonly", function () {
                         endTime = performance.now();
                         console.log(`Time to READONLY all status with "completed" :${(endTime - startTime).toFixed(2)} ms`);
-
+                        //4. Create an index on the `status` field, then measure and display the time to read all completed tasks on the console or the browser
+                        let startTimeIndex = performance.now();
+                        indexField(db, storeName, function () {
+                            let endTimeIndex = performance.now();
+                            console.log(`Time to read all objects with index: ${(endTimeIndex - startTimeIndex).toFixed(2)} ms`);
+                        });
                     });
                 });
             });
