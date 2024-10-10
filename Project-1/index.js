0176c9137500e67c479efcddb2e52f6c48a35129
Yucheng-An <yuc.an2019@gmail.com>
2024-10-10 03:39:40 -0700

    Auto-commit JS on save

--- Project-1/index.js
+++ Project-1/index.js
@@ -203,13 +203,7 @@ function main() {
 
                 });
             });
-
-            //2. Measure and display the time (in milliseconds) required to read all objects with `status` set to "completed" on the console or the browser
-            let startTime = performance.now();
-            readSomeStatusWithSomeMethod(db, storeName, "completed","readwrite", function () {
-                let endTime = performance.now();
-                console.log(`Time to READWRITE all status with "completed": ${(endTime - startTime).toFixed(2)} ms`);
-            });
+            
 
             // 3. Apply a read-only flag to the object store and measure and display the time to read all completed tasks again on the console or the browser.
             let startTimeRT = performance.now();
