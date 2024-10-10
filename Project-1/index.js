cbdcb003fdbf8a55aa7006b8acf09b4e68cc3032
Yucheng-An <yuc.an2019@gmail.com>
2024-10-10 03:37:13 -0700

    Auto-commit JS on save

--- Project-1/index.js
+++ Project-1/index.js
@@ -76,11 +76,11 @@ function add100kObjects(db, storeName, callback) {
                 //     status: `${statuses[Math.floor(Math.random() * statuses.length)]}`,
                 //     dueDate: new Date(Date.now() + Math.floor(Math.random() * 1000000000)).toISOString().split('T')[0]
                 // };
-                let object = {
-                        id: i,
-                        task: `task_${i}`,
-                        status: `${statuses[Math.floor(Math.random() * statuses.length)]}`,
-                        dueDate: new Date(Date.now() + Math.floor(Math.random() * 1000000000)).toISOString().split('T')[0]
+                // let object = {
+                //         id: i,
+                //         task: `task_${i}`,
+                //         status: `${statuses[Math.floor(Math.random() * statuses.length)]}`,
+                //         dueDate: new Date(Date.now() + Math.floor(Math.random() * 1000000000)).toISOString().split('T')[0]
                     };
                 writeObjectStore.add(object);
             }
