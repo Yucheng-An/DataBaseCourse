60c1ef220bbfe3416d8c10b500fa372966485fde
Yucheng-An <yuc.an2019@gmail.com>
2024-10-10 03:04:48 -0700

    Auto-commit JS on save

--- Project-1/index.js
+++ Project-1/index.js
@@ -152,7 +152,7 @@ function readSomeStatusWithSomeMethod(db, storeName, status, method, callback) {
         } else {
             console.log(`Found status '${status}':`, counter, " By using method:", method);
             let endTime = performance.now();
-            console.log(`Time for ${mode} mode reading completed tasks: ${(endTime - startTime).toFixed(2)} ms`);
+            console.log(`(In function timer)Time for ${mode} mode reading completed tasks: ${(endTime - startTime).toFixed(2)} ms`);
             callback();
         }
     };
