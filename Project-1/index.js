d316ab52ca190cbef41e461087c9111091909820
Yucheng-An <yuc.an2019@gmail.com>
2024-10-10 03:34:01 -0700

    Auto-commit JS on save

--- Project-1/index.js
+++ Project-1/index.js
@@ -78,7 +78,7 @@ function add100kObjects(db, storeName, callback) {
                 // };
                 let object = {
                         id: i,
-                        task: `task`,
+                        task: `task_`,
                         status: `${statuses[Math.floor(Math.random() * statuses.length)]}`,
                         dueDate: new Date(Date.now() + Math.floor(Math.random() * 1000000000)).toISOString().split('T')[0]
                     };
