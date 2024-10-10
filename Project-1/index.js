53bf88f63022162edfe70d34f0a220b179541b1d
Yucheng-An <yuc.an2019@gmail.com>
2024-10-10 03:32:07 -0700

    Auto-commit JS on save

--- Project-1/index.js
+++ Project-1/index.js
@@ -166,6 +166,7 @@ function indexField(db, storeName, callback) {
     let objectStore = transaction.objectStore(storeName);
     let index = objectStore.index("id");
     let count = 0;
+    
     let request = index.openCursor();
     request.onsuccess = function (event) {
         let cursor = event.target.result;
